31e4ac83f56c4269e51d303b704430dbb7e59802
Juntoku Shimizu <toksters@gmail.com>
2020-09-29 21:02:09 -0400

    added basic resource endpoint

--- src/main/java/com/rankerapp/resource/RankerAppResource.java
+++ src/main/java/com/rankerapp/resource/RankerAppResource.java
@@ -1,7 +1,16 @@
 package com.rankerapp.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RankerAppResource {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name") String name) {
+        System.out.println("Hello " + name);
+        return "FUCK YOU";
+    }
 
 }
