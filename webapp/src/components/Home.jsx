efb0e1b54f65e05bde66c9ca02ab3cdd17ec86fd
nirkraw <nirkaw@gmail.com>
2021-01-10 12:01:40 -0500

    working on navbar logout vs login button

--- webapp/src/components/Home.jsx
+++ webapp/src/components/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [lists, setLists] = useState([]);
    
   useEffect(() => {
-    fetchLists();
+    fetchLists(localStorage.getItem("userId"));
   },[]);
 
   const fetchLists = async () => {
--- webapp/src/components/Navbar.jsx
+++ webapp/src/components/Navbar.jsx
@@ -3,6 +3,10 @@ import "../styles/Navbar.css";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
+
+  const logout = () => {
+    localStorage.setItem("userId", "");
+  }
   return (
     <div id="nav-main">
       <NavLink to="/" id="navbar-title">
@@ -15,9 +19,19 @@ export default function Navbar() {
         <NavLink to="/create-user" className="nav-session-button">
           + New User
         </NavLink>
-        <NavLink to="/login" className="nav-session-button">
-          Login
-        </NavLink>
+        {localStorage.getItem("userId") === "" ? (
+          <NavLink to="/login" className="nav-session-button">
+            Login
+          </NavLink>
+        ) : (
+          <NavLink
+            to="/login"
+            onClick={logout}
+            className="nav-session-button"
+          >
+            Logout
+          </NavLink>
+        )}
       </div>
     </div>
   );
