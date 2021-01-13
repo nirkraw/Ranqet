a42c81414852c6093442d50149343fd823a2eb76
nirkraw <nirkaw@gmail.com>
2021-01-13 16:04:11 -0500

    add styling to homepage

--- webapp/src/components/Home.jsx
+++ webapp/src/components/Home.jsx
@@ -12,10 +12,10 @@ export default function Home() {
 
   useEffect(() => {
     fetchList();
-  },[]);
+  }, []);
 
   const fetchList = async () => {
-      try {
+    try {
       const res = await fetchTopLists();
       const currNewLists = [];
       for (let i = 0; i < res.data.topLists.length; i++) {
@@ -26,25 +26,30 @@ export default function Home() {
     } catch (err) {
       setError(err.message);
     }
-  }
+  };
 
-  if (error) return <ErrorPage error={error} />
+  if (error) return <ErrorPage error={error} />;
   if (loading) return <LoadingSpinner />;
 
   const newListLi = newLists.map((list, i) => {
     return (
-      <NavLink className="list-name" to={`/${list.id}/quiz`} key={i}>
-        {i + 1}: {list.title}
-      </NavLink>
+      <li className="top-lists-item">
+        <NavLink className="top-lists-item-name" to={`/${list.id}/quiz`} key={i}>
+          {list.title}
+        </NavLink>
+        <p className="top-lists-item-description">{list.description}</p>
+      </li>
     );
   });
 
   return (
     <div id="home-main">
-      <h1 id="home-title">Home</h1>
-      <h1 id="all-list-error">{error}</h1>
-      <h3>New Lists</h3>
-      <ul id="top-list-ul">{newListLi}</ul>
+      <div id="top-list-container">
+        <h1 id="home-title">Today's Top Lists</h1>
+        <h1 id="all-list-error">{error}</h1>
+        <ul id="top-list-ul">{newListLi}</ul>
+      </div>
+      <div id="other-container"></div>
     </div>
   );
 }
