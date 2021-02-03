794b498d3677d2301095f40d656740cb8cdb58e3
nirkraw <nirkaw@gmail.com>
2021-02-02 21:13:03 -0500

    change delete frontend

--- webapp/src/components/ListIndex.jsx
+++ webapp/src/components/ListIndex.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { fetchListOptionPair } from "../util/Endpoints";
 import { useHistory } from "react-router-dom";
 import "../styles/listIndex.css";
