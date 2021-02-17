acaf2224b9bc43f2809dcc20c59e999babe2b167
nirkraw <nirkaw@gmail.com>
2021-02-16 22:08:11 -0500

    add header for no global ranking

--- webapp/src/components/RankingsList.jsx
+++ webapp/src/components/RankingsList.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
+import React from "react";
 import "../styles/Rankings.css";
 import { useHistory, useRouteMatch } from "react-router-dom";
 
-export default function RankingsList({rankings, rankingName, openModal}) {
+export default function RankingsList({ rankings, rankingName, openModal }) {
   const history = useHistory();
   const match = useRouteMatch();
-  
-  const handleTakeQuiz = () => {
-       if (localStorage.getItem("userId")) history.push(`/${match.params.listId}/quiz`);
-       else openModal(["login", `/${match.params.listId}/quiz`]);
-  }
 
-  if(rankings.length === 0) return (
+  const handleTakeQuiz = () => {
+    if (localStorage.getItem("userId"))
+      history.push(`/${match.params.listId}/quiz`);
+    else openModal(["login", `/${match.params.listId}/quiz`]);
+  };
+
+  if (rankings.length === 0) {
+    if (rankingName !== "Global Rankings") {
+      return (
+        <div id="ranking-main-div">
+          <h2 className="ranking-header">{rankingName}</h2>
+          <button id="rankings-quiz-button" onClick={handleTakeQuiz}>
+            Login To Take the Quiz!
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div id="ranking-main-div">
+          <h2 className="ranking-header">No {rankingName} Yet</h2>
+        </div>
+      );
+    }
+  }
+  return (
     <div id="ranking-main-div">
       <h2 className="ranking-header">{rankingName}</h2>
-      <button id="rankings-quiz-button" onClick={handleTakeQuiz}>Login To Take the Quiz!</button>
+      <ul>
+        {rankings.map((ranking, i) => (
+          <li className="ranking-name" key={i}>
+            <span>{i + 1}.</span>
+            <h2>{ranking.name}</h2>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-    return (
-      <div id="ranking-main-div">
-        <h2 className="ranking-header">{rankingName}</h2>
-        <ul>
-          {rankings.map((ranking, i) => (
-            <li className="ranking-name" key={i}>
-              <span>{i + 1}.</span>
-              <h2>{ranking.name}</h2>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
 }
