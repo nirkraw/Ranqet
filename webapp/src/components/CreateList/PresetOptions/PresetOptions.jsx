12ce1075913e3ad732ee40d7330629c944f39fbe
nirkraw <nirkaw@gmail.com>
2021-02-28 17:35:55 -0500

    choose preset as checkbox and clears all other options on select

--- webapp/src/components/CreateList/PresetOptions/PresetOptions.jsx
+++ webapp/src/components/CreateList/PresetOptions/PresetOptions.jsx
@@ -9,7 +9,7 @@ export default function PresetOptions({
   isOpen,
   setPresetModalOpen,
   setOptions,
-  options
+  options,
 }) {
   const [currOptions, setCurrOptions] = useState(null);
   const basketballPlayers = [
@@ -17,16 +17,19 @@ export default function PresetOptions({
       name: "Lebron James",
       photoUrl:
         "https://media2.giphy.com/media/26vUGuV1WxhbkEKZy/200.gif?cid=51413b55f8rtbt1infu60hkdzgm7dit636vfuyu3lo8pdaar&rid=200.gif",
+      selected: false,
     },
     {
       name: "Kevin Durant",
       photoUrl:
         "https://media2.giphy.com/media/Yl5i129jGy6eblZdAb/200.gif?cid=51413b55800kt6mirecawcjjo3p74pig192j00j5qppocdow&rid=200.gif",
+      selected: false,
     },
     {
       name: "Kyrie Irving",
       photoUrl:
         "https://media4.giphy.com/media/l3V0IEJ0TLPbCCkDK/200.gif?cid=51413b55qywzlzm4j1fx34n95vx5zwhgpf73lmbsxzmktpfg&rid=200.gif",
+      selected: false,
     },
   ];
 
@@ -35,12 +38,14 @@ export default function PresetOptions({
       name: "Barak Obama",
       photoUrl:
         "https://media3.giphy.com/media/3ohA2M1VhQtitqzLP2/200.gif?cid=51413b55ulrifit245qhro016nfd68blva6iup04uxmmruat&rid=200.gif",
+      selected: false,
     },
     {
       name: "Joe Biden",
       photoUrl:
         "https://media0.giphy.com/media/HRyEK9KBJGswKuccof/200.gif?cid=51413b55y2gde4ol4n981vr8mzqx0wjsce3kj784jl0lmabc&rid=200.gif",
-    }
+      selected: false,
+    },
   ];
 
   const myPresets = [
@@ -49,8 +54,16 @@ export default function PresetOptions({
   ];
 
   const setPresetOptions = () => {
+    let optionsIdx = 0;
     for (let i = 0; i < currOptions.length; i++) {
-      options[i] = currOptions[i];
+      if (currOptions[i] && currOptions[i].selected) {
+        options[optionsIdx] = currOptions[i];
+        optionsIdx++;
+      } 
+    }
+    for (let i = optionsIdx; i < options.length; i++) {
+      options[i] = {name: "", photoUrl: ""};
+      
     }
     setOptions(options);
     setPresetModalOpen(false);
@@ -64,7 +77,10 @@ export default function PresetOptions({
             presets={myPresets}
             setCurrOptions={setCurrOptions}
           />
-          <PresetOptionsDisplay options={currOptions} />
+          <PresetOptionsDisplay
+            options={currOptions}
+            setCurrOptions={setCurrOptions}
+          />
         </div>
         <div id="preset-options-button-container">
           <button onClick={() => setPresetModalOpen(false)}>Close</button>
--- webapp/src/components/CreateList/PresetOptions/PresetOptionsDisplay.jsx
+++ webapp/src/components/CreateList/PresetOptions/PresetOptionsDisplay.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 
-export default function PresetOptionsDisplay({ options }) {
-  if (!options) return <ul id="preset-options-display-container"></ul>
+export default function PresetOptionsDisplay({ options, setCurrOptions }) {
+  if (!options) return <ul id="preset-options-display-container"></ul>;
+
+  const selectBox = (index) => {
+    const newOptions = JSON.parse(JSON.stringify(options));
+    newOptions[index].selected = !options[index].selected;
+    setCurrOptions(newOptions);
+  };
+
   return (
     <ul id="preset-options-display-container">
+      <button className="site-button">Select All</button>
       {options.map((option, i) => (
         <li id="preset-options-display-item" key={i}>
-          <h1>{option.name}</h1>
+          <div className="preset-options-checkbox-container">
+            <input
+              type="checkbox"
+              className="preset-checkbox-input"
+              name={option.name}
+              value={option.name}
+              checked={option.selected}
+              onChange={() => selectBox(i)}
+            />
+            <label className="preset-checkbox-label">{option.name}</label>
+          </div>
           <img src={option.photoUrl} alt="option" />
         </li>
       ))}
