fbcb71b684faf51199854e9003708dc3983149af
nirkraw <nirkaw@gmail.com>
2021-02-28 18:25:29 -0500

    add select all button to preset checkbox

--- webapp/src/components/CreateList/PresetOptions/PresetOptionsDisplay.jsx
+++ webapp/src/components/CreateList/PresetOptions/PresetOptionsDisplay.jsx
@@ -9,9 +9,19 @@ export default function PresetOptionsDisplay({ options, setCurrOptions }) {
     setCurrOptions(newOptions);
   };
 
+  const selectAll = () => {
+    const newOptions = JSON.parse(JSON.stringify(options));
+    for (let i = 0; i < newOptions.length; i++) {
+      newOptions[i].selected = true;
+    }
+    setCurrOptions(newOptions);
+  };
+
   return (
     <ul id="preset-options-display-container">
-      <button className="site-button">Select All</button>
+      <button className="site-button" onClick={selectAll}>
+        Select All
+      </button>
       {options.map((option, i) => (
         <li id="preset-options-display-item" key={i}>
           <div className="preset-options-checkbox-container">
