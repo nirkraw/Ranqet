ae579fcb6283414a88e162e0718e9a550d665bf5
nirkraw <nirkaw@gmail.com>
2021-03-09 14:41:25 -0500

    clean up userProfile

--- webapp/src/components/UserProfile/UserProfile.jsx
+++ webapp/src/components/UserProfile/UserProfile.jsx
@@ -6,25 +6,19 @@ import { useRouteMatch } from "react-router-dom";
 import ListIndex from "../ListIndex";
 import useCache from "../../util/useCache.js";
 
-export default function UserProfile({ userInfo, setUserInfo }) {
+export default function UserProfile() {
   const match = useRouteMatch();
-  const [userListCacheId, userListloading] = useCache({
+  const [userListCacheId, loading] = useCache({
     fn: fetchUserPublicList,
-    args: [match.params.userId]
+    args: [match.params.userId],
   });
   const data = JSON.parse(localStorage.getItem(userListCacheId));
-  if (userListloading) return <LoadingSpinner />;
+  if (loading) return <LoadingSpinner />;
 
   return (
     <div id="user-profile-main-container">
-      <UserInfo
-        numCreated={data.lists.length}
-        userInfo={userInfo}
-        setUserInfo={setUserInfo}
-      />
-      <div id="tabs-container-div">
-        <ListIndex passedList={data.lists} />
-      </div>
+      <UserInfo numCreated={data.lists.length} />
+      <ListIndex passedList={data.lists} />
     </div>
   );
 }
