ffa768444ac12083547b7deaf2b31d153ac1b0b7
nirkraw <nirkaw@gmail.com>
2021-03-14 16:36:52 -0400

    add styling to session form

--- webapp/src/components/SessionForm.jsx
+++ webapp/src/components/SessionForm.jsx
@@ -6,7 +6,6 @@ import { useHistory } from "react-router-dom";
 export default function SessionForm({ formType, openModal, route }) {
   const history = useHistory();
   const [username, setUsername] = useState("");
-  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [userError, setUserError] = useState();
   const [formHeader, setFormHeader] = useState();
@@ -14,7 +13,7 @@ export default function SessionForm({ formType, openModal, route }) {
   useEffect(() => {
     setUserError("");
     if (formType === "login") setFormHeader("Log In");
-    else setFormHeader("Sign Up");
+    else setFormHeader("Create Account");
   }, [formType]);
 
   const handleSubmit = async (e) => {
@@ -25,7 +24,7 @@ export default function SessionForm({ formType, openModal, route }) {
     }
     try {
       const endpoint = formHeader === "Sign Up" ? createUser : loginUser;
-      const res = await endpoint(name, username, password);
+      const res = await endpoint(username, password);
       localStorage.setItem("userId", res.data.id);
       localStorage.setItem("sessionToken", res.data.sessionToken);
       openModal({ formType: "", route: "" });
@@ -46,62 +45,63 @@ export default function SessionForm({ formType, openModal, route }) {
   };
 
   return (
-    <div className="session-form-container">
-      <form id="login-form" onSubmit={handleSubmit}>
-        <h3>{formHeader}</h3>
-        <input
-          type="text"
-          value={username}
-          placeholder="Username"
-          onChange={(event) => setUsername(event.target.value)}
-          className="login-input"
-        />
-        {formHeader === "Sign Up" ? (
+    <form id="session-form" onSubmit={handleSubmit}>
+      <h3>{formHeader}</h3>
+      <div id="session-inputs-container" className="justify-column-center">
+        <div className="session-input-container">
+          <p>Username</p>
           <input
             type="text"
-            value={name}
-            placeholder="Name"
-            onChange={(event) => setName(event.target.value)}
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
             className="login-input"
+            maxlength="30"
           />
-        ) : (
-          <div></div>
-        )}
-        <input
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange={(event) => setPassword(event.target.value)}
-          className="login-input"
-        />
-        <div className="session-buttons-container">
-          {formType === "login" ? (
-            <div
-              className="switch-session-button"
-              onClick={(e) => {
-                e.preventDefault();
-                openModal({ formType: "signup", route: route});
-              }}
-            >
-              Sign Up
-            </div>
-          ) : (
-            <div
-              className="switch-session-button"
-              onClick={(e) => {
-                e.preventDefault();
-                openModal({ formType: "login", route: route });
-              }}
-            >
-              Log In
-            </div>
-          )}
-          <button id="session-submit" type="submit">
-            Continue
-          </button>
         </div>
-        <h2 className="session-errors">{userError}</h2>
-      </form>
-    </div>
+        <div className="session-input-container">
+          <p>Password</p>
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="login-input"
+            maxlength="24"
+          />
+        </div>
+      </div>
+      <div className="session-submit-container">
+        <div id="session-submit" type="submit">
+          {formType === "login" ? "Log In" : "Sign Up"}
+        </div>
+      </div>
+      {formType === "login" ? (
+        <div className="justify-center-center switch-session-container">
+          <p className="switch-session-text">New to Ranqet?</p>
+          <p
+            className="switch-session-button"
+            onClick={(e) => {
+              e.preventDefault();
+              openModal({ formType: "signup", route: route });
+            }}
+          >
+            Create an account.
+          </p>
+        </div>
+      ) : (
+        <div className="justify-center-center switch-session-container">
+          <p className="switch-session-text">Already a Ranqet user?</p>
+          <p
+            className="switch-session-button"
+            onClick={(e) => {
+              e.preventDefault();
+              openModal({ formType: "login", route: route });
+            }}
+          >
+            Log In.
+          </p>
+        </div>
+      )}
+      <h2 className="session-errors">{userError}</h2>
+    </form>
   );
 }
