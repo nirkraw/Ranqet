36adc9afc8275feea21e5572526068b1f1a0c4fb
nirkraw <nirkaw@gmail.com>
2021-03-19 23:38:07 -0400

    add styling to list upload image

--- webapp/src/components/CreateList/ListUploadImage.js
+++ webapp/src/components/CreateList/ListUploadImage.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "../../styles/createList/ListUploadImage.css";
+import Web from "../../assets/web.png";
+import Picture from "../../assets/picture.png";
 
 export default function ListUploadImage() {
   return (
     <div className="list-upload-image-main justify-column-center">
       <h2 className="create-list-label">Photo</h2>
       <div className="list-upload-image justify-start-center">
-        <div className="list-upload-uploadImage"></div>
-        <div className="list-upload-searchGiffy"></div>
+        <div className="list-upload-uploadImage column-center-center">
+          <img className="list-upload-icon" src={Picture} alt="web"></img>
+          <p className="list-upload-type-header">Upload Image</p>
+        </div>
+        <div className="list-upload-searchGiffy column-center-center">
+          <img className="list-upload-icon-2" src={Web} alt="web"></img>
+          <p className="list-upload-type-header">Search Giffy</p>
+        </div>
       </div>
     </div>
   );
