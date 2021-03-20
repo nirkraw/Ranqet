4c532fe395ef66cd8685dee5af5afd23385c9958
nirkraw <nirkaw@gmail.com>
2021-03-20 13:20:34 -0400

    add hover to listuploadimage

--- webapp/src/components/CreateList/ListUploadImage.js
+++ webapp/src/components/CreateList/ListUploadImage.js
@@ -4,12 +4,20 @@ import Web from "../../assets/web.png";
 import Picture from "../../assets/picture.png";
 
 export default function ListUploadImage() {
+  const uploadFile = () => {};
   return (
     <div className="list-upload-image-main justify-column-center">
       <h2 className="create-list-label">Photo</h2>
       <div className="list-upload-image justify-start-center">
-        <div className="list-upload-uploadImage column-center-center">
-          <img className="list-upload-icon" src={Picture} alt="web"></img>
+        <div
+          onClick={uploadFile}
+          className="list-upload-uploadImage column-center-center"
+        >
+          <img
+            className="list-upload-icon"
+            src={Picture}
+            alt="generic-icon"
+          ></img>
           <p className="list-upload-type-header">Upload Image</p>
         </div>
         <div className="list-upload-searchGiffy column-center-center">
