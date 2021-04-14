4fa077549a769fdf9e5e6ab1e739ed8e35b84981
nirkraw <nirkaw@gmail.com>
2021-04-13 20:45:30 -0400

    Erase preset from create list

--- webapp/src/components/CreateList/CreateList.jsx
+++ webapp/src/components/CreateList/CreateList.jsx
@@ -7,7 +7,6 @@ import ListUploadImage from "./ListUploadImage";
 import CategoriesDropdown from "./CategoriesDropdown";
 import UnlistedDropdown from "./UnlistedCheckbox";
 import { useHistory } from "react-router-dom";
-import Tooltip from "../Tooltip";
 import { clearEndpointCache } from "../../util/clearEndpointCache";
 import { fetchUserPublicList } from "../../util/Endpoints/UserEP";
 
@@ -22,8 +21,6 @@ export default function CreateList() {
   const [userError, setUserError] = useState("");
   const [loading, setLoading] = useState(false);
   const [presetModalOpen, setPresetModalOpen] = useState(false);
-  const [savePresets, setSavePresets] = useState(false);
-  const [presetTitle, setPresetTitle] = useState("");
 
   useEffect(() => {
     window.scroll({ top: 0, left: 0, behavior: "smooth" });
@@ -36,8 +33,6 @@ export default function CreateList() {
     else if (listOptions.length < 2)
       setUserError("Please add at least two options.");
     else if (!category) setUserError("Please choose category.");
-    else if (savePresets && !presetTitle)
-      setUserError("Please add a preset title");
     else {
       const visitedOptions = new Set();
       for (let i = 0; i < listOptions.length; i++) {
@@ -61,8 +56,7 @@ export default function CreateList() {
           listOptions,
           localStorage.getItem("userId"),
           unlisted,
-          category,
-          presetTitle
+          category
         );
         setLoading(false);
         clearEndpointCache(fetchUserPublicList, [
@@ -121,49 +115,12 @@ export default function CreateList() {
       </div>
       <div id="create-list-options-div">
         <h1 className="create-list-title">List Options</h1>
-        <div id="infotag-and-preset-button-container">
-          <button
-            id="option-preset-button"
-            onClick={(e) => {
-              e.preventDefault();
-              setPresetModalOpen(true);
-            }}
-          >
-            Use Preset Options
-          </button>
-          <Tooltip
-            helpText={
-              "Prefill your options with previously made public or personal options"
-            }
-          />
-        </div>
         <ListOptions
           setListOptions={setListOptions}
           presetModalOpen={presetModalOpen}
           setPresetModalOpen={setPresetModalOpen}
           setUserError={setUserError}
         />
-        <div id="infotag-and-preset-button-container">
-          <label id="unlisted-label">Save Options As Preset:</label>
-          <input
-            type="checkbox"
-            id="unlisted-input"
-            name="unlisted"
-            onChange={() => setSavePresets(!savePresets)}
-          />
-          <Tooltip helpText={"Save your options as a preset for future use"} />
-        </div>
-        {savePresets ? (
-          <div className="preset-input-container">
-            <label className="preset-input-label">Preset Name</label>
-            <input
-              id="preset-privacy-input"
-              maxLength="32"
-              type="text"
-              onChange={(e) => setPresetTitle(e.target.value)}
-            />
-          </div>
-        ) : null}
       </div>
       <button onClick={handleListSubmit} id="create-list-submit">
         Create List
