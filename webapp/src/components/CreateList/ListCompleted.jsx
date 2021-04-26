c3c0e8778f2d218e9ffae4a1a049eadbebf18b11
nirkraw <nirkaw@gmail.com>
2021-04-25 21:12:33 -0400

    add completed list new designs

--- webapp/src/components/CreateList/ListCompleted.jsx
+++ webapp/src/components/CreateList/ListCompleted.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useHistory, useRouteMatch } from "react-router-dom";
 import { fetchList } from "../../util/Endpoints/ListEP";
 import LoadingSpinner from "../Misc/LoadingSpinner";
+import "../../styles/createList/listCompleted.css";
 
 export default function ListCompleted() {
   const linkRef = useRef(null);
@@ -9,26 +10,32 @@ export default function ListCompleted() {
   const [title, setTitle] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [description, setDescription] = useState("");
+  const [author, setAuthor] = useState("");
+  const [authorId, setAuthorId] = useState("");
   const history = useHistory();
   const match = useRouteMatch();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     loadList();
-  }, [])
+  }, []);
 
-  const loadList = async () =>  {
+  const loadList = async () => {
     try {
       const res = await fetchList(match.params.listId);
-      if (res.data.createdBy.id !== localStorage.getItem("userId")) history.push("/");
+      debugger
+      if (res.data.createdBy.id !== localStorage.getItem("userId"))
+        history.push("/");
       setTitle(res.data.title);
       setDescription(res.data.description);
       setImageUrl(res.data.imageUrl);
+      setAuthor(res.data.createdBy.username);
+      setAuthorId(res.data.createdBy.id);
       setLoading(false);
-    } catch(err) {
+    } catch (err) {
       history.push(`/error/${err.message}`);
     }
-  }
+  };
 
   const copyToClipboard = (e) => {
     navigator.clipboard.writeText(linkRef.current.value);
@@ -37,41 +44,60 @@ export default function ListCompleted() {
 
   if (loading) return <LoadingSpinner />;
   return (
-    <div id="list-completed-main">
-      <div id="list-completed-sub">
-        <div id="list-completed-info-and-image">
-          {imageUrl ? <img className="list-image" src={imageUrl} alt="list"></img> : null}
-          <div id="list-completed-info">
-            <p>{title}</p>
-            <p>{description}</p>
+    <div className="list-completed-main column-start">
+      <h1 className="list-completed-main-header">List Created!</h1>
+      <div className="list-completed-sub justify-start">
+        <div className="list-completed-info-and-button">
+          <div id="list-completed-info-and-image">
+            <div id="list-completed-info">
+              <p>{title}</p>
+              <div
+                className="list-index-author"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  history.push(`/profile/${authorId}`);
+                }}
+              >
+                <span>By: </span>
+                <p>{author}</p>
+              </div>
+              <p>{description}</p>
+            </div>
+          </div>
+          <div className="list-completed-buttons">
+            <input
+              ref={linkRef}
+              id="share-url-input"
+              disabled
+              value={`${window.location.origin}/${match.params.listId}/quiz`}
+            />
+            <button className="site-button" onClick={copyToClipboard}>
+              Copy Link
+            </button>
+            <div
+              className="site-button-2"
+              onClick={() => history.push(`/${match.params.listId}/quiz`)}
+            >
+              Ranqet
+            </div>
+            <p
+              className={
+                !active
+                  ? "copied-message"
+                  : "copied-message copied-message-active"
+              }
+            >
+              Copied!
+            </p>
           </div>
         </div>
-        <div id="list-completed-buttons">
-          <input
-            ref={linkRef}
-            id="share-url-input"
-            disabled
-            value={`${window.location.origin}/${match.params.listId}/quiz`}
-          />
-          <button className="site-button" onClick={copyToClipboard}>
-            Copy Link
-          </button>
-          <button
-            className="site-button"
-            onClick={() => history.push(`/${match.params.listId}/quiz`)}
-          >
-            Rank it!
-          </button>
-          <p
-            className={
-              !active
-                ? "copied-message"
-                : "copied-message copied-message-active"
-            }
-          >
-            Copied!
-          </p>
-        </div>
+        {imageUrl ? (
+          <img
+            className="list-completed-list-image"
+            src={imageUrl}
+            alt="list"
+          ></img>
+        ) : null}
       </div>
     </div>
   );
