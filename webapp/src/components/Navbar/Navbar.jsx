7c92ca2144d15284b617ac617fc5f7b274c4faf5
Juntoku Shimizu <toksters@gmail.com>
2021-05-04 21:40:20 -0400

    semicolon in navbar

--- webapp/src/components/Navbar/Navbar.jsx
+++ webapp/src/components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar() {
                 },
               });
               window.dispatchEvent(openModal); //eventListener in SessionModal
-            };}
+            }}
           >
             Sign Up
           </div>
