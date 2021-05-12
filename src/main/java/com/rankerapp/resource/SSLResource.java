52774751ed71e982aab6b8d67a2204cd2423f83c
Juntoku Shimizu <toksters@gmail.com>
2021-05-11 21:49:12 -0400

    allow all origins

--- src/main/java/com/rankerapp/resource/SSLResource.java
+++ src/main/java/com/rankerapp/resource/SSLResource.java
@@ -1,5 +1,6 @@
 package com.rankerapp.resource;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SSLResource {
     
+    @CrossOrigin(origins = "*")
     @GetMapping("/.well-known/acme-challenge/{key}")
     public String returnChecksumResponse(@PathVariable(value = "key") String key) {
         return "3JLfyMdtyYIQDuoVdk0yre5QlXsI6mJKtJPqvHGg1Hk.5iaLsA_8UuwhIXgaCgzY32KP8RRLIhRo-GWe0vvuvUw";
