562e9b21f36634d90871aba6cf1b54fa9daa7086
Juntoku Shimizu <toksters@gmail.com>
2021-05-11 20:03:41 -0400

    return status code using app error controller

--- src/main/java/com/rankerapp/exceptions/RedirectException.java
+++ src/main/java/com/rankerapp/exceptions/RedirectException.java
@@ -3,6 +3,6 @@ package com.rankerapp.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.CONTINUE)
+@ResponseStatus(code = HttpStatus.MOVED_PERMANENTLY)
 public class RedirectException extends RuntimeException {
 }
