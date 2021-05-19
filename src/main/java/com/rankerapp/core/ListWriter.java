c4b6e3d4a97481ea6fc83f6a3bedad853df9959d
Juntoku Shimizu <toksters@gmail.com>
2021-05-18 20:14:45 -0400

    cleaned up ListWriter.java

--- src/main/java/com/rankerapp/core/ListWriter.java
+++ src/main/java/com/rankerapp/core/ListWriter.java
@@ -10,6 +10,7 @@ import com.rankerapp.exceptions.ForbiddenException;
 import com.rankerapp.exceptions.NotFoundException;
 import com.rankerapp.transport.model.ListCategory;
 import com.rankerapp.transport.model.SubmittedOption;
+import org.apache.commons.lang3.ObjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -45,59 +46,24 @@ public class ListWriter {
 
     public ListEntity createList(String title, String description, UUID authorId, List<SubmittedOption> options,
                                  UUID imageId, String imageUrl, ListCategory category, boolean isPrivate) {
-        if (category == null) {
-            throw new BadRequestException("List category is required!");
-        } else if (category == ListCategory.NEW || category == ListCategory.POPULAR) {
-            throw new BadRequestException("Invalid ListCategory: " + category);
-        }
-
-        // validate that two options don't have the same name
-        Set<String> optionNames = new HashSet<>();
-        for (SubmittedOption option : options) {
-            if (optionNames.contains(option.getName())) {
-                throw new BadRequestException("Request has duplicate option names!");
-            }
-            optionNames.add(option.getName());
-        }
-
-        if (authorId == null) {
-            throw new BadRequestException("Author ID required to create list!");
-        }
-
-        UserEntity author;
-        try {
-            author = usersRepository.getOne(authorId);
-        } catch (EntityNotFoundException e) {
-            throw new NotFoundException("Author with provided id not found");
-        }
-
-        if (options == null || options.size() < 2) {
-            throw new BadRequestException("At least two options must be present to create a list");
-        }
-
-        ListEntity listEntity = new ListEntity();
-        listEntity.setId(UUID.randomUUID());
-        listEntity.setTitle(title);
-        listEntity.setDescription(description);
-        List<OptionEntity> optionEntities = options.stream()
-                .map((submittedOption) -> new OptionEntity(submittedOption.getName(), listEntity, submittedOption.getImageUrl()))
-                .collect(Collectors.toList());
-        for (int i = 0; i < optionEntities.size(); i++) {
-            optionEntities.get(i).setOptionNumber(i + 1);
-        }
-        listEntity.setOptions(optionEntities);
-        listEntity.setCreatedOn(Instant.now());
-        listEntity.setCreatedBy(author);
-        listEntity.setImageUrl(imageUrl);
-        listEntity.setPrivate(isPrivate);
-        listEntity.setCategory(com.rankerapp.db.model.ListCategory.valueOf(category.name()));
+        validateCategory(category);
+        validateOptions(options);
+        
+        // build and write list to db
+        UserEntity author = Optional.ofNullable(authorId)
+                .map(this::findAuthorById)
+                .orElseThrow(() -> new BadRequestException("Author ID required to create list!"));
+        com.rankerapp.db.model.ListCategory categoryDBEnum = com.rankerapp.db.model.ListCategory.valueOf(category.name());
+        ListEntity listEntity = buildListEntity(title, description, options, author, categoryDBEnum, imageUrl, isPrivate);
         listsRepository.save(listEntity);
         
+        // link images used in this list with the newly created listId
         List<UUID> imageIdsToLink = options.stream()
                 .filter((option) -> !StringUtils.isEmpty(option.getImageId()))
                 .map((option) -> UUID.fromString(option.getImageId()))
                 .collect(Collectors.toList());
         
+        // link title image
         if (imageId != null) {
             imageIdsToLink.add(imageId);
         }
@@ -117,6 +83,8 @@ public class ListWriter {
         list.setPrivate(!list.isPrivate());
     }
     
+    // Fetch all images and create new image link records for all of them with the new list id
+    // This will be used as a reference counter to determine which images can be cleaned up by async process
     private void linkImagesWithListId(UUID listId, List<UUID> imageIds) {
         List<ImageListLinkEntity> imageLinks = imageRecordsRepository.findAllById(imageIds).stream()
                 .map((image) -> image.getId())
@@ -131,6 +99,60 @@ public class ListWriter {
                 .collect(Collectors.toList());
         
         imageListLinksRepository.saveAll(imageLinks);
+    }
+    
+    private void validateCategory(ListCategory category) {
+        if (category == null) {
+            throw new BadRequestException("List category is required!");
+        } else if (category == ListCategory.NEW || category == ListCategory.POPULAR) {
+            throw new BadRequestException("Invalid ListCategory: " + category);
+        }
+    }
+    
+    private void validateOptions(List<SubmittedOption> options) {
+        if (options == null || options.size() < 2) {
+            throw new BadRequestException("At least two options must be present to create a list");
+        }
+    
+        Set<String> optionNames = new HashSet<>();
+        for (SubmittedOption option : options) {
+            if (optionNames.contains(option.getName())) {
+                throw new BadRequestException("Request has duplicate option names!");
+            }
+            optionNames.add(option.getName());
+        }
+    }
+    
+    private UserEntity findAuthorById(UUID authorId) {
+        try {
+            return usersRepository.getOne(authorId);
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundException("Author with provided id not found");
+        }
+    }
+    
+    private ListEntity buildListEntity(String title, String description, List<SubmittedOption> options, UserEntity author,
+                                       com.rankerapp.db.model.ListCategory category, String imageUrl, boolean isPrivate) {
+        ListEntity listEntity = new ListEntity();
+        listEntity.setId(UUID.randomUUID());
+        listEntity.setTitle(title);
+        listEntity.setDescription(description);
+        List<OptionEntity> optionEntities = options.stream()
+                .map((submittedOption) -> new OptionEntity(submittedOption.getName(), listEntity, submittedOption.getImageUrl()))
+                .collect(Collectors.toList());
+        
+        // Manually set option numbers in order they were received
+        for (int i = 0; i < optionEntities.size(); i++) {
+            optionEntities.get(i).setOptionNumber(i + 1);
+        }
+        
+        listEntity.setOptions(optionEntities);
+        listEntity.setCreatedOn(Instant.now());
+        listEntity.setCreatedBy(author);
+        listEntity.setImageUrl(imageUrl);
+        listEntity.setPrivate(isPrivate);
+        listEntity.setCategory(category);
+        return listEntity;
     }
 
 }
